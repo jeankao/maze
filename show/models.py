b7d62afed319c94c8af7315308b01b51e8f8fc01
koma <koma@mail.nksh.tp.edu.tw>
2017-08-06 00:16:33 -0400

    update

--- show/models.py
+++ show/models.py
@@ -3,4 +3,4 @@ from django.utils import timezone
 
 class Record(models.Model):
         seconds = models.IntegerField(default=0)
-        publication_date = models.DateTimeField(default=timezone.now())
+        publication_date = models.DateTimeField(default=timezone.now)
