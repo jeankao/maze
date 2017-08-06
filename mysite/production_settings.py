efaa488cd43515294af1d1c17ed8146cdacd1a4a
koma <koma@mail.nksh.tp.edu.tw>
2017-08-05 20:23:29 -0400

    update

--- mysite/production_settings.py
+++ mysite/production_settings.py
@@ -9,9 +9,6 @@ DATABASES = {
         'default': dj_database_url.config()
 }
 
-# Static asset configuration.
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure().
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
